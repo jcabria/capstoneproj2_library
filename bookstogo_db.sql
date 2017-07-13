1b97bc190975da1976828bc0e3e9de0d39a8382b
jcabria <julie.cabria@gmail.com>
2017-07-13 16:48:08 +0800

    update date

--- bookstogo_db.sql
+++ bookstogo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2017 at 04:10 AM
+-- Generation Time: Jul 13, 2017 at 10:43 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.2
 
